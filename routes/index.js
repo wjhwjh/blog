118e60225f6a8dc7dce14ce79561fbf7e903d7da
wjhwjh <1468017149@qq.com>
2018-06-08 15:48:06 +0800

    添加登录页面，并写相应的路由器

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+//var app = express();  // app和router区别 都可以用来调用路由的，区别
+
 
 /* GET home page. */
+/*req  是用来请求数据的
+* res  是做响应的
+* */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/logIn', function (req, res, next) {
+  res.render('logIn');
 });
 
 module.exports = router;
